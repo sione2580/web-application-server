6010f9f2219664125efdec221854d60549d14742
ahn <attki22@gmail.com>
2019-04-15 12:29:11 +0900

    3번에서 web만 commit push

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -20,9 +20,6 @@ public class WebServer {
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         System.out.println("GIT BRANCH");
-        System.out.println("devBranch2는  testDev에서 만든 두번째 브랜치()");
-        System.out.println("devBranch2는  testDev에서 만든 두번째 브랜치의 두번째 커밋()");
-        System.out.println("devBranch3는  testDev에서 만든 세번째 브랜치의 첫번째 커밋()");
         
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
